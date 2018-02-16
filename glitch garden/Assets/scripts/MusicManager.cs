150758e76d5ead5d0d42187825f3bda212edc086
hakbar0 <haseeb2000@hotmail.com>
2018-02-16 19:27:15 +0000

    Made music player.
    Made a music array holder. Which plays music depending on which level it is.

---
+++ glitch garden/Assets/scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake (){
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't Destroy on load: " + name);
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMuisc = levelMusicChangeArray[level];
+			Debug.Log ("Playing clip:" + thisLevelMuisc);
+
+		if (thisLevelMuisc) {
+			audioSource.clip = thisLevelMuisc;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+
+}
